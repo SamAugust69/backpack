4dc58cb9bf3798cac5021b5f333f0c7dfa6f4f33
SamAugust69 <sammonsonis@gmail.com>
2024-05-14 21:16:21 -0400

    fart

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 
-//const inter = Inter({ subsets: ['latin'] });
-const roboto = Roboto({ weight: ['100', '300', '400', '500', '700', '900'], subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -18,7 +17,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${roboto.className} flex flex-col min-h-[90vh] bg-neutral-950`}>
+			<body className={`${inter.className} flex flex-col min-h-[90vh] bg-neutral-950`}>
 				<Navbar />
 				{children}
 			</body>
